7617de8cbed3aea596cbc72aaf0c738a71a935df
LReva <lenareva338@gmail.com>
2024-01-28 19:01:07 -0500

    KAN-41 / enabled swiping on single photo view and fixed back and next buttons positioning

--- app/javascript/components/BackAndNextNavigation.jsx
+++ app/javascript/components/BackAndNextNavigation.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useTheme } from '@mui/material';
 import { IconButton, Box } from '@mui/material';
 import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 
 
 const BackAndNextNavigation = ({index, photos, parentLocation}) => {
+  const theme = useTheme();
   const navigate = useNavigate();
     
     const handleBackOnePhoto = () => {
@@ -17,7 +19,6 @@ const BackAndNextNavigation = ({index, photos, parentLocation}) => {
     }
     
         return (
-
             <Box style={{
               position: 'absolute',
               bottom: 15,
@@ -30,10 +31,18 @@ const BackAndNextNavigation = ({index, photos, parentLocation}) => {
               maxWidth: '300px', 
           }}>
               <IconButton size="medium" onClick= {handleBackOnePhoto} style={{ color: 'white', flex: 1}} disabled={index === 0} >
+              {theme.direction === 'rtl' ? (
+                <KeyboardArrowRight />
+            ) : (
                 <KeyboardArrowLeft />
+            )}
               </IconButton>
               <IconButton size="medium" onClick={handleNextOnePhoto} style={{ color: 'white', flex: 1}} disabled={index === photos.length - 1}>
-                < KeyboardArrowRight/>
+              {theme.direction === 'rtl' ? (
+                <KeyboardArrowLeft />
+            ) : (
+                <KeyboardArrowRight />
+            )}
               </IconButton>
             </Box>
         );
--- app/javascript/components/PhotoView.jsx
+++ app/javascript/components/PhotoView.jsx
@@ -1,14 +1,28 @@
 import React, {useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useTheme } from '@mui/material';
 import { IconButton, Button, Box } from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import FullscreenIcon from '@mui/icons-material/Fullscreen';
 import FullscreenExitIcon from '@mui/icons-material/FullscreenExit';
 import BackAndNextNavigation from './BackAndNextNavigation';
 import AutoPlayNavigation from './AutoPlayNavigation';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import { useSwipeable } from 'react-swipeable';
 import {importImage} from '../photoLoader.js'
 
+const localTheme = createTheme({
+  palette: {
+    primary: {
+      main: 'rgb(0,0,0)'
+    },
+  }, 
+});
+
 const PhotoView = ({photo, photos, index }) => {
+  const theme = useTheme();
   const navigate = useNavigate();
   const location = useLocation();
   const [parentLocation, setParentLocation] = useState('/photos')
@@ -30,52 +44,104 @@ const PhotoView = ({photo, photos, index }) => {
     navigate(`${parentLocation}`)
   }
 
-  return (
-    <Box sx={{position:'relative'}}>
-      <Box sx= {{display:'flex'}}>
-        <Button
-          startIcon ={<ArrowBackIcon/>}
-          sx = {{color: 'black',
-                fontFamily: 'Nanum Brush Script'}}
-          onClick={handleGoBackButton}
-          >
-          Go Back
-        </Button>
-        <IconButton
-          color="inherit"
-          sx={{marginLeft: 'auto'}}
+  const handleBackOnePhoto = () => {
+    if (index > 0) {
+    navigate(`/photos/${photos[index-1].id}`, {state: { parentLocation: parentLocation}})
+    }
+  }
+  
+  const handleNextOnePhoto = () => {
+    if (index < photos.length - 1) {
+    navigate(`/photos/${photos[index+1].id}`, {state: {parentLocation: parentLocation}})
+    }
+  }
 
-          onClick={() => setIsFullScreen(true)}
-          >
-          <FullscreenIcon/>
-        </IconButton>
-        <AutoPlayNavigation index={index} photos={photos} parentLocation={parentLocation}/>
-      </Box>
-        <img src={imagePath} 
-          alt={photo.name} 
-          style={{                     
-          height: '80vh',
-          display: 'block',
-          overflow: 'hidden',
-          width: '100vw',
-          objectFit: 'contain'}} 
-        />
-        <BackAndNextNavigation index={index} photos={photos} parentLocation={parentLocation}/>
-        {isFullScreen && (
-          <Box className="fullScreenModal">
-            <IconButton
-              color="inherit"
-              onClick={() => setIsFullScreen(false)}
-              style={{ position: 'absolute', top: 20, right: 20 }}
+  const handlers = useSwipeable({
+    onSwipedLeft: () => handleNextOnePhoto(),
+    onSwipedRight: () => handleBackOnePhoto(),
+    preventDefaultTouchmoveEvent: true,  // Prevents scrolling while swiping)
+    trackMouse: true // Track mouse swipes on non-touch devices
+  });
+
+  return (
+    <ThemeProvider theme={localTheme}>
+      <Box sx={{position:'relative'}}>
+        <Box sx= {{display:'flex'}}>
+          <Button
+            startIcon ={<ArrowBackIcon/>}
+            sx = {{color: 'black',
+                  fontFamily: 'Nanum Brush Script'}}
+            onClick={handleGoBackButton}
             >
-              <FullscreenExitIcon style={{ color: 'white' }} />
-            </IconButton>
-            <AutoPlayNavigation index={index} photos={photos} parentLocation={parentLocation} style={{ position: 'absolute', top: 50, right: 20 }}/>
-            <BackAndNextNavigation index={index} photos={photos} parentLocation={parentLocation}/>
-            <img src={imagePath} alt={photo.name} style={{ maxWidth: '100%', maxHeight: '100%' }} />
-          </Box>
-        )}
-  </Box>
+            Go Back
+          </Button>
+          <IconButton
+            color="inherit"
+            sx={{marginLeft: 'auto'}}
+
+            onClick={() => setIsFullScreen(true)}
+            >
+            <FullscreenIcon/>
+          </IconButton>
+          <AutoPlayNavigation index={index} photos={photos} parentLocation={parentLocation}/>
+        </Box>
+        <Box sx={{display: 'flex',
+                            flexDirection: 'row',
+                            alignContent: 'center'}}>
+          <Button 
+            sx ={{display: {xs: 'none', md: 'flex'}}}
+            size="large" 
+            onClick={handleBackOnePhoto} 
+            disabled={index === 0}>
+            {theme.direction === 'rtl' ? (
+                <KeyboardArrowRight />
+            ) : (
+                <KeyboardArrowLeft />
+            )}
+          </Button>
+          <img src={imagePath} 
+            {...handlers}
+            alt={photo.name} 
+            style={{                     
+            height: '80vh',
+            display: 'block',
+            overflow: 'hidden',
+            width: '100vw',
+            objectFit: 'contain'}} 
+          />
+          <Button 
+            sx ={{display: {xs: 'none', md: 'flex'}}}
+            size="large" 
+            onClick={handleNextOnePhoto} 
+            disabled={index === 0}>
+            {theme.direction === 'rtl' ? (
+                <KeyboardArrowLeft />
+            ) : (
+                <KeyboardArrowRight />
+            )}
+          </Button>
+        </Box>
+          {isFullScreen && (
+            <Box className="fullScreenModal">
+              <IconButton
+                color="inherit"
+                onClick={() => setIsFullScreen(false)}
+                style={{ position: 'absolute', top: 20, right: 20 }}
+              >
+                <FullscreenExitIcon style={{ color: 'white' }} />
+              </IconButton>
+              <AutoPlayNavigation index={index} photos={photos} parentLocation={parentLocation} style={{ position: 'absolute', top: 50, right: 20 }}/>
+              <BackAndNextNavigation index={index} photos={photos} parentLocation={parentLocation}/>
+              <img 
+                  src={imagePath}
+                  {...handlers}
+                  alt={photo.name}
+                  style={{ maxWidth: '100%', 
+                           maxHeight: '100%' }} />
+            </Box>
+          )}
+    </Box>
+  </ThemeProvider>
   );
 };
 
