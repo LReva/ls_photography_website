a6627cc50b968bea4745524e76fd99fe96927ebf
LReva <lenareva338@gmail.com>
2024-02-02 18:22:45 -0500

    KAN-48 / navigation and full screen view fixed in photo view

--- app/javascript/components/AutoPlayNavigation.jsx
+++ app/javascript/components/AutoPlayNavigation.jsx
@@ -1,5 +1,4 @@
-import React, {useState, useEffect} from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
 import { IconButton, Box } from '@mui/material';
 import PlayCircleFilledWhiteIcon from '@mui/icons-material/PlayCircleFilledWhite';
 import StopCircleIcon from '@mui/icons-material/StopCircle';
@@ -13,66 +12,30 @@ const localTheme = createTheme({
   }, 
 });
 
-const AutoPlayNavigation = ({index, photos, parentLocation, style}) => {
-    const [autoPlayEnabled, setAutoPlayEnabled] = useState(false)
-    const navigate = useNavigate();
-    const [activeStep, setActiveStep] = useState(index);
-    const photoCount = photos.length
-
-    useEffect(() => {
-      setActiveStep(index)
-    }, [index]);
-
-    useEffect(() => {
-        let interval;
+const AutoPlayNavigation = ({autoPlayEnabled, toggleAutoplay, style}) => {
       
-        if (autoPlayEnabled) {
-          interval = setInterval(() => {
-            setActiveStep((prevIndex) => {
-              const nextIndex = prevIndex === photoCount-1 ? 0 : prevIndex + 1;
-              return nextIndex;
-            });
-          }, 4000);
-        }
-      
-        return () => clearInterval(interval);
-      }, [autoPlayEnabled, photoCount]);
-
-    useEffect(() => {
-        if (autoPlayEnabled) {
-            navigate(`/photos/${photos[activeStep].id}`, {state: {parentLocation: parentLocation}});
-        }
-    }, [autoPlayEnabled, activeStep, navigate]);
-    
-    const toggleAutoplay = () => {
-        setAutoPlayEnabled(!autoPlayEnabled);
-        if (!autoPlayEnabled) {
-          navigate(`/photos/${photos[activeStep].id}`, {state: {parentLocation: parentLocation}});
-        }
-      };
-      
-        return (
-          <ThemeProvider theme={localTheme}>
-            <Box  style={style}>
-                  { !autoPlayEnabled &&
-                    <IconButton
-                      color="primary"
-                      onClick={toggleAutoplay}
-                    >
-                      <PlayCircleFilledWhiteIcon  />
-                    </IconButton >           
-                  }
-                  { autoPlayEnabled &&
-                    <IconButton
-                      color="primary"
-                      onClick={toggleAutoplay}
-                    >
-                      <StopCircleIcon  />
-                    </IconButton>
-                  }
-            </Box>
-        </ThemeProvider>
-        );
+      return (
+        <ThemeProvider theme={localTheme}>
+          <Box  style={style}>
+                { !autoPlayEnabled &&
+                  <IconButton
+                    color="primary"
+                    onClick={toggleAutoplay}
+                  >
+                    <PlayCircleFilledWhiteIcon  />
+                  </IconButton >           
+                }
+                { autoPlayEnabled &&
+                  <IconButton
+                    color="primary"
+                    onClick={toggleAutoplay}
+                  >
+                    <StopCircleIcon  />
+                  </IconButton>
+                }
+          </Box>
+      </ThemeProvider>
+      );
     };
 
 export default AutoPlayNavigation;
